349bcb57154a7145a9765a1729e578bb63485ed9
Loyce <chenty02@qq.com>
2023-11-11 20:15:18 +0800

    FIFO Chisel Code

---
+++ src/main/scala/FIFO/Main.scala
@@ -0,0 +1,92 @@
+package FIFO
+
+import chisel3._
+
+class CompareFIFOs(MSBD: Int, LAST: Int, MSBA: Int) extends Module {
+  val io = IO(new Bundle {
+    val dataIn = Input(UInt((MSBD+1).W))
+    val push = Input(Bool())
+    val pop = Input(Bool())
+    val equal = Output(Bool())
+  })
+  val sr = Module(new srFIFO(MSBD, LAST, MSBA))
+  val rb = Module(new rbFIFO(MSBD, LAST, MSBA))
+  sr.io.dataIn := io.dataIn
+  sr.io.push := io.push
+  sr.io.pop := io.pop
+  rb.io.dataIn := io.dataIn
+  rb.io.push := io.push
+  rb.io.pop := io.pop
+
+  io.equal := (sr.io.full === rb.io.full) &&
+    (sr.io.empty && rb.io.empty) &&
+    (sr.io.empty || sr.io.dataOut === rb.io.dataOut)
+}
+
+class srFIFO(MSBD: Int, LAST: Int, MSBA: Int) extends Module {
+  val io = IO(new Bundle() {
+    val dataIn = Input(UInt((MSBD + 1).W))
+    val push = Input(Bool())
+    val pop = Input(Bool())
+    val dataOut = Output(UInt((MSBD + 1).W))
+    val full = Output(Bool())
+    val empty = Output(Bool())
+  })
+  val mem = RegInit(VecInit.fill(LAST + 1)(0.U((MSBD + 1).W)))
+  val tail = RegInit(0.U((MSBD + 1).W))
+  val empty = RegInit(true.B)
+
+  when(io.push && !io.full) {
+    for (i <- LAST until 0 by -1) {
+      mem(i) := mem(i - 1)
+    }
+    mem(0) := io.dataIn
+    when(!empty) {
+      tail := tail + 1.U
+    }
+    empty := false.B
+  }.elsewhen(io.pop && !empty) {
+    when(tail === 0.U) {
+      empty := true.B
+    }.otherwise {
+      tail := tail - 1.U
+    }
+  }
+  io.dataOut := mem(tail)
+  io.full := tail === LAST.U
+  io.empty := empty
+}
+
+
+class rbFIFO(MSBD: Int, LAST: Int, MSBA: Int) extends Module {
+  val io = IO(new Bundle() {
+    val dataIn = Input(UInt((MSBD + 1).W))
+    val push = Input(Bool())
+    val pop = Input(Bool())
+    val dataOut = Output(UInt((MSBD + 1).W))
+    val full = Output(Bool())
+    val empty = Output(Bool())
+  })
+  val mem = RegInit(VecInit.fill(LAST + 1)(0.U((MSBD + 1).W)))
+  val head = RegInit(0.U((MSBD + 1).W))
+  val tail = RegInit(0.U((MSBD + 1).W))
+  val empty = RegInit(true.B)
+
+  when(io.push && !io.full) {
+    mem(head) := io.dataIn
+    head := head + 1.U
+    empty := false.B
+  }.elsewhen(io.pop && !empty) {
+    when(tail === head) {
+      empty := true.B
+    }
+    tail := tail + 1.U
+  }
+  io.dataOut := mem(tail)
+  io.full := tail === head && !empty
+  io.empty := empty
+}
+
+object Main extends App {
+  emitVerilog(new CompareFIFOs(3, 15, 3), Array("--target-dir", "generated"))
+}
--- src/test/scala/Barrel/BarrelTest.scala
+++ src/test/scala/Barrel/BarrelTest.scala
@@ -7,10 +7,6 @@ class BarrelTest extends AnyFlatSpec with ChiselScalatestTester {
   "Barrel" should "pass" in {
     test(new Barrel(4, 4)) { barrel => {
       barrel.clock.step()
-      println(barrel.io.test0.peekInt())
-      println(barrel.io.test1.peekInt())
-      println(barrel.io.test2.peekInt())
-      println(barrel.io.test3.peekInt())
     }}
   }
 }
